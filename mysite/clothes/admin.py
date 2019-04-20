048093fdb767b8ea0bbf15f88e05adfa2b354f6f
Leonid <fokusnik229@gmail.com>
2019-04-20 23:01:20 +0400

    Auto slug
    Auto slug filling based on Item name

--- mysite/clothes/admin.py
+++ mysite/clothes/admin.py
@@ -4,7 +4,7 @@ from .models import Item
 
 
 class ItemAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("name",)}
+    prepopulated_fields = {"slug": ("name", )}
 
 
-admin.site.register(Item)
+admin.site.register(Item, ItemAdmin)
