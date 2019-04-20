7b55c60c71cc97932431cb8fb3d0e7e2cc232023
Leonid <fokusnik229@gmail.com>
2019-04-21 02:38:26 +0400

    Item cards
    All Items are displaying on home page. 
    BUG: Item image isn`t displaying!

--- mysite/clothes/views.py
+++ mysite/clothes/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, render
+from .models import Item
 
-# Create your views here.
 
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
@@ -9,7 +9,14 @@ from django.utils import timezone
 
 
 def index(request):
-    return render(request, 'clothes/index.html')
+    categoies = Item.CATEGORIES
+    items = Item.objects.all()
+    context = {
+        'categories': categoies,
+        'items': items,
+    }
+
+    return render(request, 'clothes/index.html', context)
 
 
 def hello_world(request):
